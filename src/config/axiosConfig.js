f48632c9f793a2df67fcdce3d1f6d1a17dadf4ff
Aayush Panchal <ayupanchal00@gmail.com>
2025-10-03 11:07:59 +0530

    Updated project with new changes

--- src/config/axiosConfig.js
+++ src/config/axiosConfig.js
@@ -2,7 +2,7 @@
 import axios from "axios";  // must be used below
 
 const API = axios.create({
-    baseURL: process.env.REACT_APP_API_URL,
+    baseURL: process.env.REACT_APP_API_BASE_URL,
 });
 
 export default API;
